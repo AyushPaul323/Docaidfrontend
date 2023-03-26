c1bd588853f2fa8d509567e9ddda6f5ef3bf4ef4
AYUSH PAUL <104216792+AyushPaul323@users.noreply.github.com>
2023-03-26 13:24:11 +0530

    Update index.jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 function Login() {
 	const googleAuth = () => {
 		window.open(
-			`http://localhost:8080/auth/google/callback`,
+			`https://docaidbackend.onrender.com/auth/google/callback`,
 			"_self"
 		);
 	};
